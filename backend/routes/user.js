6cc9ab853a7e0d7064df4177d4ca1d020e156e60
CoderXYZ14 <s1404islam@gmail.com>
2024-04-18 19:54:56 +0530

    Signin  bakcend auth routes

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -41,4 +41,36 @@ router.post("/signup", async (req, res) => {
   });
 });
 
+const signinSchema = zod.object({
+  username: zod.string().email,
+  password: zod.string(),
+});
+router.post("/signin", async (req, res) => {
+  const body = req.body;
+  const { success } = signinSchema.safeParse(req.body);
+  if (!success) {
+    return res.status(411).json({
+      message: "Incorrwct input",
+    });
+  }
+  const dbUser = await User.findOne({
+    username: body.username,
+    password: body.username,
+  });
+  if (dbUser) {
+    const token = jwt.sign(
+      {
+        userId: dbUser._id,
+      },
+      JWT_SECRET
+    );
+    res.json({
+      token: token,
+    });
+    return;
+  }
+  res.status(411).json({
+    message: "Error while loggin in",
+  });
+});
 module.exports = router;
