d81a76d81d07c30138df29a38e8f2f0b7d68a895
CoderXYZ14 <s1404islam@gmail.com>
2024-04-19 03:15:49 +0530

    new routes for accounts

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const userRouter = require("./user");
+const accountRouter = require("./account");
 const router = express.Router();
 
 app.use("./user", userRouter);
+app.use("./account", accountRouter);
 
 module.exports = router;
