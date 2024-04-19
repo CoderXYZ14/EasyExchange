28b7b518add70c6c56099deabfc4c16a9623e0bc
CoderXYZ14 <s1404islam@gmail.com>
2024-04-20 02:04:08 +0530

    Added appbar

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -11,9 +11,9 @@ export const Signup = () => {
         <div className="rounded-lg bg-white w-80 text-center p-2 h-max px-4">
           <Heading label={"Sign up"} />
           <SubHeading label={"Enter your infromation to create an account"} />
-          <InputBox placeholder="John" label={"First Name"} />
-          <InputBox placeholder="Doe" label={"Last Name"} />
-          <InputBox placeholder="harkirat@gmail.com" label={"Email"} />
+          <InputBox placeholder="Virat" label={"First Name"} />
+          <InputBox placeholder="Kohli" label={"Last Name"} />
+          <InputBox placeholder="abcd123@gmail.com" label={"Email"} />
           <InputBox placeholder="123456" label={"Password"} />
           <div className="pt-4">
             <Button label={"Sign up"} />
